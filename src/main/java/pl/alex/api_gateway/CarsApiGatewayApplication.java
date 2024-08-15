917593aaaf5157d491f4a8796f31d42e6ca4b58d
Alex <oleksandr.bodnarchuk@co3.eu>
2024-08-15 17:34:31 +0200

    created api gateway and registered routes for my clients

---
+++ src/main/java/pl/alex/api_gateway/CarsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package pl.alex.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsApiGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CarsApiGatewayApplication.class, args);
+  }
+
+}
